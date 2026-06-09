812f830418017f289b393db95dfd1bf3fb68f6ac
Yakir Rojas Azaña <yakir.rojasa@unmsm.edu.pe>
2026-06-09 01:22:23 -0500

    Agregar saludo especial

--- mi-primer-proyecto/hello.py
+++ mi-primer-proyecto/hello.py
@@ -4,5 +4,8 @@ def saludar(nombre):
 def despedida(nombre):
     return f"Adios, {nombre}"
 
+def saludo_especial(nombre):
+    return f"Bienvenido, {nombre}. Somos la mejor facultad de UNMSM."
+
 if __name__ == "__main__":
     print(saludar("UNMSM"))
